bc8d278a351050ea0937f2497460a5d76714845c
demian <demian@kcd.co.kr>
2021-02-24 22:58:33 +0900

    web - Remove unused code

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -7,21 +7,11 @@
 // 'src/pages/HomePage/HomePage.js'         -> HomePage
 // 'src/pages/Admin/BooksPage/BooksPage.js' -> AdminBooksPage
 
-import { Private, Router, Route } from '@redwoodjs/router'
+import { Router, Route } from '@redwoodjs/router'
 
 const Routes = () => {
   return (
     <Router>
-      <Route path="/contact" page={ContactPage} name="contact" />
-      <Route path="/blog-post/{id:Int}" page={BlogPostPage} name="blogPost" />
-      <Route path="/" page={HomePage} name="home" />
-      <Private unauthenticated="home">
-        <Route path="/admin/posts/new" page={NewPostPage} name="newPost" />
-        <Route path="/admin/posts/{id:Int}/edit" page={EditPostPage} name="editPost" />
-        <Route path="/admin/posts/{id:Int}" page={PostPage} name="post" />
-        <Route path="/admin/posts" page={PostsPage} name="posts" />
-        <Route path="/about" page={AboutPage} name="about" />
-      </Private>
       <Route notfound page={NotFoundPage} />
     </Router>
   )
